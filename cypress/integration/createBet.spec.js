f4178f53d70662c43d21c1dfc31847b27c58c253
André Lacerda <andrlacerda180@gmail.com>
2022-01-24 10:57:38 -0300

    test: ensure to add correct numbers in cart

---
+++ cypress/integration/createBet.spec.js
@@ -0,0 +1,20 @@
+/// <reference types="cypress" />
+
+describe('Create bet', () => {
+  it('should add correct numbers in cart', () => {
+    window.localStorage.setItem('authTokenLottery', 'token')
+    cy.visit('http://localhost:3000/newbet')
+    cy.get('.jIKGnW').click()
+    cy.get('.sc-ikJyIC > :nth-child(1)').click()
+    cy.get('button.sc-dPiLbb.euYQMT.selected').invoke('text').then(numbers => {
+      const selectedNumbers = numbers.split('').reduce((acc, num, index) => {
+        if(index === numbers.length - 1){
+          return acc + num
+        }
+        return index % 2 === 1 ? acc + num + ',' : acc + num
+      }, '')
+      cy.get('.sc-hiCibw').click()
+      cy.get('.sc-cidDSM > p').invoke('text').should('include', selectedNumbers)
+    })
+  })
+})
