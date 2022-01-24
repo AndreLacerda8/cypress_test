96065abee75cc330b1b23fae1b1b38417cc94125
André Lacerda <andrlacerda180@gmail.com>
2022-01-24 09:51:28 -0300

    test: create user successfully

--- cypress/integration/createUser.spec.js
+++ cypress/integration/createUser.spec.js
@@ -15,4 +15,34 @@ describe('Create User', () => {
     cy.get('.sc-fKVqWL > .sc-hGPBjI').click()
     cy.get('.sc-ezbkAF').contains('Email already exists')
   })
+
+  it('should navigate to /dashboard when all data is correct', () => {
+    cy.visit('http://localhost:3000/registration')
+    cy.get('[placeholder="Name"]').type('Name')
+    cy.get('[placeholder="Email"]').type('test@mail.com')
+    cy.get('[placeholder="Password"]').type('password')
+    cy.intercept('POST', 'user/create', {
+      statusCode: 200,
+      body: {
+        user: {
+          email: "test@mail.com",
+          name: "Name",
+          created_at: new Date().toISOString(),
+          updated_at: new Date().toISOString(),
+          id: 7
+        },
+        token: {
+          type: "bearer",
+          token: "token",
+          expires_at: new Date(Date.now() + 1000*60*60*24)
+        }
+      }
+    })
+    cy.intercept('GET', 'all-bets', {
+      statusCode: 200,
+      body: []
+    })
+    cy.get('.sc-fKVqWL > .sc-hGPBjI').click()
+    cy.url().should('include', '/dashboard')
+  })
 })
